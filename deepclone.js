f114ac71c1f561b0d082322014217b12800dc840
jim <473723204@qq.com>
2022-08-19 17:31:58 +0800

    1

--- deepclone.js
+++ deepclone.js
@@ -1,14 +1,42 @@
 // 1. JOSN.stringify()/JSON.parse()
 let obj = {a: 1, b: {x: 3}}
+let obj1 = 1
 JSON.parse(JSON.stringify(obj))
 
+const target = {
+  field1: 1,
+  field2: undefined,
+  field3: {
+      child: 'child'
+  },
+  field4: [2, 4, 8]
+};
+target.target = target;
+
 // 2. 递归拷贝
-function deepClone(obj) {
-  let copy = obj instanceof Array ? [] : {}
-  for (let i in obj) {
-    if (obj.hasOwnProperty(i)) {
-      copy[i] = typeof obj[i] === 'object' ? deepClone(obj[i]) : obj[i]
+function deepClone(obj,map = new Map()) {
+  if(typeof obj === 'object'){
+    if(map.get(target)){
+      return map.get(target)
     }
+    let targetObj = Array.isArray(obj) ? [] : {}
+    for(let i in obj){
+      targetObj[i] = deepClone(obj[i])
+    }
+    map.set(target,targetObj)
+    return targetObj
+  }else{
+    return obj
   }
-  return copy
+
 }
+// function deepClone(obj) {
+//   let targetObj = {}
+//   for(let i in obj){
+//     targetObj[i] = obj[i]
+//   }
+//   return targetObj
+// }
+
+
+deepClone(obj)
